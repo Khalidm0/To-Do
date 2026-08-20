8db2a68446c87c136bb6d1c5777f03f2be8906cc
khaled <khaledmohamedahmed123@gmail.com>
2026-08-20 14:22:24 +0300

    Update TaskStore.ts

--- src/store/TaskStore.ts
+++ src/store/TaskStore.ts
@@ -1,20 +1,55 @@
-import {create} from "zustand";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
+const useTaskStore = create(
+  persist(
+    (set: any) => ({
+      tasks: [],
+      searchQuery: "",
+      editingTask: null,
 
-const useTaskStore=create((set:any)=>({
-    tasks:[],
-    searchQuery: "",
-    editingTask: null,
+      setTasks: (tasks: any) => set({ tasks }),
+      setSearchQuery: (searchQuery: any) => set({ searchQuery }),
+      setEditingTask: (editingTask: any) => set({ editingTask }),
 
-    setTasks:(tasks:any)=>set({tasks}),
-    setSearchQuery:(searchQuery:any)=>set({searchQuery}),
-    setEditingTask:(editingTask:any)=>set({editingTask}),
+      addTask: (task: any) =>
+        set((state: any) => ({
+          tasks: [
+            ...state.tasks,
+            {
+              ...task,
+              id: Date.now(),
+              completed: false,
+            },
+          ],
+        })),
 
-    addTask: (task:any) =>set((state:any) => ({tasks: [...state.tasks,{...task,id: Date.now(),completed: false,},],})),
-    deleteTask: (id:any) =>set((state:any) => ({ tasks: state.tasks.filter((task:any) => task.id !== id) })),
-    updateTask: (updatedTask:any) =>set((state:any) => ({tasks: state.tasks.map((task:any) =>task.id === updatedTask.id ? updatedTask : task),  editingTask: null,})),
-    completeTask: (id:any) =>set((state:any) => ({tasks: state.tasks.map((task:any) =>task.id === id ? { ...task, completed: !task.completed } : task),})),
+      deleteTask: (id: any) =>
+        set((state: any) => ({
+          tasks: state.tasks.filter((task: any) => task.id !== id),
+        })),
 
-    }));
+      updateTask: (updatedTask: any) =>
+        set((state: any) => ({
+          tasks: state.tasks.map((task: any) =>
+            task.id === updatedTask.id ? updatedTask : task
+          ),
+          editingTask: null,
+        })),
 
-    export default useTaskStore;  
+      completeTask: (id: any) =>
+        set((state: any) => ({
+          tasks: state.tasks.map((task: any) =>
+            task.id === id
+              ? { ...task, completed: !task.completed }
+              : task
+          ),
+        })),
+    }),
+    {
+      name: "todo-storage",
+    }
+  )
+);
+
+export default useTaskStore;
